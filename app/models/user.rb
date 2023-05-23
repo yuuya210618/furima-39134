bd6cd9839359077619d352181bc39a355054f150
yuuya210618 <yuuya210618@gmail.com>
2023-05-23 18:07:23 +0900

    モデル・テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,6 @@ class User < ApplicationRecord
 
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
+  has_many :items
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_17_093631) do
+ActiveRecord::Schema.define(version: 2023_05_23_084610) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "product_name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "price", null: false
+    t.integer "shipping_charge_id", null: false
+    t.integer "shipping_origin_id", null: false
+    t.integer "derivary_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -36,4 +51,5 @@ ActiveRecord::Schema.define(version: 2023_05_17_093631) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
