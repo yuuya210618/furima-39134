2ac1f153500fd5d55eb5493c6928677b1df41b68
yuuya210618 <yuuya210618@gmail.com>
2023-06-01 17:29:53 +0900

    destroyアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
 
   def index
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+  
+  def destroy
+   @item.destroy
+   redirect_to root_path
   end
 
   private
