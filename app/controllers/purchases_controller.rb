a942df7b2444a735262a0741d73b89626380472d
yuuya210618 <yuuya210618@gmail.com>
2023-06-03 15:32:49 +0900

    購入フォーム実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,13 +4,5 @@ class PurchasesController < ApplicationController
     @order = Order.new
   end
 
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:post_code, :shipping_origin_id, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id]])
-  end
-
 end
 
