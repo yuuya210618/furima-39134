18dfe06f30efac2b2d30382bba41f3de9cffb518
yuuya210618 <yuuya210618@gmail.com>
2023-06-08 19:48:07 +0900

    クレジットカード決済機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,8 +10,10 @@ class PurchasesController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
+    amount = @item.price
     @order = Order.new(purchase_params)
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
@@ -23,6 +25,18 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:order).permit(:post_code, :shipping_origin_id, :mayor_ward_village,
-                                  :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+                                  :address, :building, :telephone_number)
+          .merge(token: params[:token],
+                 user_id: current_user.id,
+                 item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: amount,  
+      card: purchase_params[:token],    
+      currency: 'jpy'                 
+    )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,13 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :post_code, :shipping_origin_id, :mayor_ward_village, :address, :building, :telephone_number, :user_id, :item_id
+  attr_accessor :post_code, :shipping_origin_id, :mayor_ward_village, :address, :building, :telephone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :mayor_ward_village
     validates :address
     validates :telephone_number, format: { with: /\d{10,11}/ }, length: { maximum: 11 }
+    validates :token
     validates :user_id
     validates :item_id
   end
