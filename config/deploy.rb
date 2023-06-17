2e82516c71c0bd26f2bd4cbec295a93b17d09a49
yuuya210618 <yuuya210618@gmail.com>
2023-06-17 18:03:27 +0900

    コード修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid for current version and patch releases of Capistrano
-lock "3.17.3"
+lock '3.17.3'
 
-set :application, "furima-39134"
+set :application, 'furima-39134'
 
-set :repo_url,  'git@example.com:yuuya210618/furima-39134.git'
+set :repo_url,  'git@github.com:yuuya210618/furima-39134.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 
 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
