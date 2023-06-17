0e4a22bfa47b6359e8622df591b86256c96bab1e
yuuya210618 <yuuya210618@gmail.com>
2023-06-17 14:17:49 +0900

    段落修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,8 @@ lock "~> 3.17.3"
 set :application, "furima-39134"
 set :repo_url, "git@example.com:yuuya210618/furima-39134.git"
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 
+'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
